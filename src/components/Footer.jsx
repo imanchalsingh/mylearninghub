81a3bc8eca7a9c1ce318f5472e5c7e1fa40f2ce1
imanchalsingh <singhanchal159357@gmail.com>
2025-11-09 20:55:21 +0530

    updated.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,8 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer className="bg-gray-100 dark:bg-gray-800 text-gray-600 dark:text-gray-400 py-6 hover:bg-gray-200 dark:hover:bg-gray-700 transition-colors duration-300">
-            <div className="container mx-auto px-4 text-center">
-                © 2024 My Learning Hub. All rights reserved.
+            <div className="container mx-auto px-4 text-center font-['Kanit']">
+               ANCHAL SiNGH © 2025 My Learning Hub. All rights reserved.
             </div>
         </footer>
     );
