d5f56a187f100a3f22cb83608224137f7d6f99f5
imanchalsingh <singhanchal159357@gmail.com>
2026-02-15 21:06:47 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import React from 'react'
 import CodingProfiles from './components/CodingProfiles'
-import Footer from './components/Footer'
-import HomePage from './components/HomePage'
-
 
 function App() {
 
 
   return (
     <div className="min-h-screen bg-linear-to-br from-gray-50 via-cyan-50 to-blue-50 dark:from-gray-900 dark:via-gray-800 dark:to-gray-900 transition-colors duration-300">
-      <HomePage/>
       <CodingProfiles />
-      <Footer />
     </div>
   )
 }
